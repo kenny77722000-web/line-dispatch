e8d4ddce9e636d49b0a5f69b54e12f119dffda06
kenny77722000-web <kenny77722000@gmail.com>
2026-04-20 21:54:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+app.use(express.json());
+
 app.post("/line/webhook", async (req, res) => {
   const events = req.body.events;
 
@@ -5,10 +11,8 @@ app.post("/line/webhook", async (req, res) => {
     if (event.type !== "message") continue;
 
     const text = event.message.text;
-
     console.log("使用者說:", text);
 
-    // ⭐ 回覆 LINE
     await fetch("https://api.line.me/v2/bot/message/reply", {
       method: "POST",
       headers: {
@@ -28,4 +32,8 @@ app.post("/line/webhook", async (req, res) => {
   }
 
   res.sendStatus(200);
+});
+
+app.listen(10000, () => {
+  console.log("Server running on port 10000");
 });
